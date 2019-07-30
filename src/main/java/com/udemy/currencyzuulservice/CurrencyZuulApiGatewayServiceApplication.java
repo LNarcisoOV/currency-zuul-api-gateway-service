680200c47362bbb9e01bb839253d2eae27027e60
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-30 08:44:39 -0300

    Enabling zuul and eureka, and putting initial configurations.

--- src/main/java/com/udemy/currencyzuulservice/CurrencyZuulApiGatewayServiceApplication.java
+++ src/main/java/com/udemy/currencyzuulservice/CurrencyZuulApiGatewayServiceApplication.java
@@ -2,7 +2,11 @@ package com.udemy.currencyzuulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CurrencyZuulApiGatewayServiceApplication {
 
