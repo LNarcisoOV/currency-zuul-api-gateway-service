00cdc1bf2d7acd1b16a2019e30f8da1aa47e4180
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-30 08:34:34 -0300

    Adding project to the repository.

---
+++ src/main/java/com/udemy/currencyzuulservice/CurrencyZuulApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.currencyzuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyZuulApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyZuulApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/currencyzuulservice/CurrencyZuulApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.currencyzuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyZuulApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
